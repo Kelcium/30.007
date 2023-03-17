272f176e1a6c46881ce6c7b5802af04e3eedc24f
lee.yanhan@hotmail.com <ethan_gs@me.com>
2023-03-17 14:15:25 +0800

    Belt Loop Added

---
+++ Version 0/BeltLoop.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+
+#program called with RFID as arg
+reader = SimpleMFRC522()
+GPIO.setmode(GPIO.Board)
+GPIO.setup({motorpin}, GPIO.OUT)
+GPIO.setup({laserpin}, GPIO.IN)
+
+while True:
+    GPIO.output({motorpin}, GPIO.HIGH)
+    if GPIO.input({laserpin}):
+        GPIO.output({motorpin}, GPIO.LOW)
+        id, text = reader.read()
+        if id == arg:
+            break
+        else:
+            GPIO.output({motorpin}. GPIO.HIGH)
