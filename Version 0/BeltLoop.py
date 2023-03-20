52342e6237696f91dee00e82808b6b317d1fd103
Kelcium <lee.yanhan8631@gmail.com>
2023-03-20 20:57:19 +0800

    Version 1 Update

--- Version 0/BeltLoop.py
+++ Version 0/BeltLoop.py
@@ -1,4 +1,5 @@
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
+from functions import passenger
 import MFRC522
 import signal
 import time
@@ -12,85 +13,20 @@ tray_present = GPIO.input({laserpin})
 
 kiosk_read = True
 
+#connecting to Firestore, if init doesn't do it properly
+#db = firestore.client()
+
 #object of kiosk reader
 #kiosk_reader = MFRC522.MFRC522()
 
 while kiosk_read == True and tray_present == False:
+    kiosk_read = False
 
-    id, text = reader.read()
+    id, passenger = reader.read()
 
-    if id in db
-        
+    data = db.collection.document(text).get("luggage")
+    print(data)
 
-
-
-
-
-
- 
-continue_reading = True
- 
-# Capture SIGINT for cleanup when the script is aborted
-def end_read(signal,frame):
-    global continue_reading
-    print ("Ctrl+C captured, ending read.")
-    continue_reading = False
     GPIO.cleanup()
- 
-# Hook the SIGINT
-signal.signal(signal.SIGINT, end_read)
- 
-# Create an object of the class MFRC522
-MIFAREReader = MFRC522.MFRC522()
- 
-# This loop keeps checking for chips. If one is near it will get the UID and authenticate
-while continue_reading:
-    
-    # Scan for cards    
-    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
- 
-    # If a card is found
-    if status == MIFAREReader.MI_OK:
-        print ("Card detected")
-    
-    # Get the UID of the card
-    (status,uid) = MIFAREReader.MFRC522_Anticoll()
- 
-    # If we have the UID, continue
-    if status == MIFAREReader.MI_OK:
- 
-        # Print UID
-        print ("Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])+','+str(uid[4]))  
-        # This is the default key for authentication
-        key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
-        
-        # Select the scanned tag
-        MIFAREReader.MFRC522_SelectTag(uid)
-        
-        #ENTER Your Card UID here
-        my_uid = [61,84,4,114,31]
-        
-        #Configure LED Output Pin
-        LED = 18
-        GPIO.setup(LED, GPIO.OUT)
-        GPIO.output(LED, GPIO.LOW)
-        
-        #Check to see if card UID read matches your card UID
-        if uid == my_uid:                #Open the Doggy Door if matching UIDs
-            print("Access Granted")
-            GPIO.output(LED, GPIO.HIGH)  #Turn on LED
-            time.sleep(5)                #Wait 5 Seconds
-            GPIO.output(LED, GPIO.LOW)   #Turn off LED
-            
-        else:                            #Don't open if UIDs don't match
-            print("Access Denied, YOU SHALL NOT PASS!")
-        
-##        # Authenticate
-##        status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
-##
-##        # Check if authenticated
-##        if status == MIFAREReader.MI_OK:
-##            MIFAREReader.MFRC522_Read(8)
-##            MIFAREReader.MFRC522_StopCrypto1()
-##        else:
-##            print "Authentication error"
+
+
---
+++ Version 1/app/functions.py
@@ -0,0 +1,12 @@
+class passenger():
+
+    def __init__(self):
+        self.luggage_queue = []
+        pass
+
+    def is_empty(self, array):
+        if array == []:
+            return True
+
+    @luggage_queue.setter
+    def 
