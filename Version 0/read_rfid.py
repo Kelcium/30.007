a7344e2720daa613cf10d18a37d3ac40c34199e2
Kelcium <lee.yanhan@hotmail.com>
2023-03-21 16:11:55 +0800

    testing

--- Version 0/read_rfid.py
+++ Version 0/read_rfid.py
@@ -2,12 +2,25 @@
 
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
 
 reader = SimpleMFRC522()
+cred = credentials.Certificate(r"test-5b286-firebase-adminsdk-prj2f-ad65922631.json")
+firebase_admin.initialize_app(cred, {'databaseURL' : 'https://test-5b286-default-rtdb.asia-southeast1.firebasedatabase.app/'})
+ref = db.reference("/").get()
 
-try:git com
-        id, text = reader.read()
-        print(id)
-        print(text)
-finally:
-        GPIO.cleanup()
+while True:
+
+        try:
+                id, passportnum = reader.read()
+        finally:
+                GPIO.cleanup()
+
+        if passportnum in ref.keys():
+                #run storage file with passportnum as argument
+        else:
+                print("No baggage to collect")
+
+
