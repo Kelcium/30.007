40a1f6f40fb8ed99d3e064dad4a4b345494c9a15
Kelcium <lee.yanhan8631@gmail.com>
2023-03-22 06:56:52 +0000

    motor control

--- Version 0/motor_control.py
+++ Version 0/motor_control.py
@@ -1,71 +1,54 @@
-#!/usr/bin/python3
 import RPi.GPIO as GPIO
+# https://sourceforge.net/p/raspberry-gpio-python/wiki/PWM/
+
+
 import time
- 
-out1 = 17
-out2 = 18
-out3 = 27
-out4 = 22
- 
-# careful lowering this, at some point you run into the mechanical limitation of how quick your motor can move
-step_sleep = 0.002
- 
-step_count = 200
- 
-# setting up
-GPIO.setmode( GPIO.BCM )
-GPIO.setup( out1, GPIO.OUT )
-GPIO.setup( out2, GPIO.OUT )
-GPIO.setup( out3, GPIO.OUT )
-GPIO.setup( out4, GPIO.OUT )
- 
-# initializing
-GPIO.output( out1, GPIO.LOW )
-GPIO.output( out2, GPIO.LOW )
-GPIO.output( out3, GPIO.LOW )
-GPIO.output( out4, GPIO.LOW )
- 
- 
-def cleanup():
-    GPIO.output( out1, GPIO.LOW )
-    GPIO.output( out2, GPIO.LOW )
-    GPIO.output( out3, GPIO.LOW )
-    GPIO.output( out4, GPIO.LOW )
-    GPIO.cleanup()
-    print("cleanup complete")
- 
- 
-# the meat
-try:
-    i = 0
-    for i in range(step_count):
-        print("running")
-        if i%4==0:
-            GPIO.output( out4, GPIO.HIGH )
-            GPIO.output( out3, GPIO.LOW )
-            GPIO.output( out2, GPIO.LOW )
-            GPIO.output( out1, GPIO.LOW )
-        elif i%4==1:
-            GPIO.output( out4, GPIO.LOW )
-            GPIO.output( out3, GPIO.LOW )
-            GPIO.output( out2, GPIO.HIGH )
-            GPIO.output( out1, GPIO.LOW )
-        elif i%4==2:
-            GPIO.output( out4, GPIO.LOW )
-            GPIO.output( out3, GPIO.HIGH )
-            GPIO.output( out2, GPIO.LOW )
-            GPIO.output( out1, GPIO.LOW )
-        elif i%4==3:
-            GPIO.output( out4, GPIO.LOW )
-            GPIO.output( out3, GPIO.LOW )
-            GPIO.output( out2, GPIO.LOW )
-            GPIO.output( out1, GPIO.HIGH )
- 
-        time.sleep( step_sleep )
- 
-except KeyboardInterrupt:
-    cleanup()
-    exit( 1 )
- 
-cleanup()
-exit( 0 )
+
+
+# We are going to use the BCM not BOARD layout
+# https://i.stack.imgur.com/yHddo.png - BCM are the "GPIO#" ones not the ordered pins
+GPIO.setmode(GPIO.BCM)  
+GPIO.setwarnings(False)
+
+
+RPWM = 17;  # GPIO pin 17 to the RPWM on the BTS7960
+LPWM = 18;  # GPIO pin 28 to the LPWM on the BTS7960
+
+# For enabling "Left" and "Right" movement
+L_EN = 27;  # connect GPIO pin 27 to L_EN on the BTS7960
+R_EN = 22;  # connect GPIO pin 22 to R_EN on the BTS7960
+
+
+# Set all of our PINS to output
+GPIO.setup(RPWM, GPIO.OUT)
+GPIO.setup(LPWM, GPIO.OUT)
+GPIO.setup(L_EN, GPIO.OUT)
+GPIO.setup(R_EN, GPIO.OUT)
+
+
+# Enable "Left" and "Right" movement on the HBRidge
+GPIO.output(R_EN, True)
+GPIO.output(L_EN, True)
+
+
+rpwm= GPIO.PWM(RPWM, 100)
+lpwm= GPIO.PWM(LPWM, 100)
+
+rpwm.ChangeDutyCycle(0)
+
+rpwm.start(0)
+
+while 1:
+
+  for x in range(100):
+    print("Speeding up " + str(x))
+    rpwm.ChangeDutyCycle(x)
+    time.sleep(0.25)
+
+  time.sleep(5)
+
+  for x in range(100):
+
+    print("Slowing down " + str(x))
+    rpwm.ChangeDutyCycle(100-x)
+    time.sleep(0.25)
